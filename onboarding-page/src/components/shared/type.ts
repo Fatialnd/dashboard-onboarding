1fd3ee3ab769f37fd73f6a64605345d10aa35c81
Fatima <75587225+Fatialnd@users.noreply.github.com>
2023-07-11 16:46:20 +0200

    type fixes

--- onboarding-page/src/components/shared/type.ts
+++ onboarding-page/src/components/shared/type.ts
@@ -1,12 +1,12 @@
 import { type } from "os";
 import { ReactNode } from "react";
 export type ItemsType = {
-    link: string;
-    title: string;
-}
+  link: string;
+  title: string;
+};
 
-export interface AccordionData {
+export type AccordionData = {
   svgicon: ReactNode;
   title: string;
   content: ReactNode;
-} 
+};
